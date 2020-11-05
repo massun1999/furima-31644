569c4c41f00f5e6fcda7735cc0d7787339eea163
massun1999 <73097630+massun1999@users.noreply.github.com>
2020-11-05 11:11:24 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :nickname, presence: true
+         with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/} do
+          validates :last_name
+          validates :first_name
+         end
+         with_options presence: true, format: {with: /\A[ァ-ヶー－]+\z/} do
+           validates :last_name_ruby
+           validates :first_name_ruby
+         end
+         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+         validates :password, format: { with: PASSWORD_REGEX }, length: { minimum: 6 }
+         validates :email, uniqueness: true
+         validates :birthday, presence: true
+
 end
