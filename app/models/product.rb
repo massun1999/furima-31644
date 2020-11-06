48216d0eccff280843d59de8d65a8f59312fcfbb
massun1999 <73097630+massun1999@users.noreply.github.com>
2020-11-06 19:32:57 +0900

    ActiveHashの実装

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,11 @@
 class Product < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :postage
+  belongs_to :source
+  belongs_to :shipping_day
+
 
   belongs_to :user
   has_one_attached :image
@@ -6,9 +13,10 @@ class Product < ApplicationRecord
   validates :image, presence: true
   validates :name, presence:true
   validates :explanation, presence: true
-  validates :category_id, presence: true
-  validates :condition_id, presence:true
-  validates :postage_id, presence:true
-  validates :source_id, presence: true
+  validates :category_id, numericality: { other_than: 1 } 
+  validates :condition_id, numericality: { other_than: 1 } 
+  validates :postage_id, numericality: { other_than: 1 } 
+  validates :source_id, numericality: { other_than: 1 } 
+  validates :shipping_day_id, numericality: { other_than: 1 } 
   validates :price, presence: true, format: { with: /\A[0-9]+\z/ }
 end
