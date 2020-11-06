c92c3312645cc2ded8e4e47056c49e8a2fa131aa
massun1999 <73097630+massun1999@users.noreply.github.com>
2020-11-06 15:01:24 +0900

    バリデーションの設定とアソシエーションの設定

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,13 @@
 class Product < ApplicationRecord
+
+  belongs_to :user
+  has_one_attached :image
+
+  validates :name, presence:true
+  validates :explanation, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence:true
+  validates :postage, presence:true
+  validates :souces_id, presence: true
+  validates :price, presence: true, format: { with: /\A[0-9]+\z/ }
 end
