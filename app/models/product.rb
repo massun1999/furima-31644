e04404358ada428a670b4a1ab7cbcad3fd127b7d
massun1999 <73097630+massun1999@users.noreply.github.com>
2020-11-07 14:57:28 +0900

    バリデーションをwith optionsでまとめた

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,17 +10,20 @@ class Product < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  with_options presence: true
+  with_options presence: true do
     validates :image
     validates :name
     validates :explanation
   end
 
-  validates :category_id, numericality: { other_than: 1 } 
-  validates :condition_id, numericality: { other_than: 1 } 
-  validates :postage_id, numericality: { other_than: 1 } 
-  validates :source_id, numericality: { other_than: 1 } 
-  validates :shipping_day_id, numericality: { other_than: 1 } 
+  with_options presence: true do
+    validates :category_id
+    validates :condition_id 
+    validates :postage_id
+    validates :source_id
+    validates :shipping_day_id
+  end 
+
   validates :price, presence: true, format: { with: /\A[0-9]+\z/ },
             numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 end
