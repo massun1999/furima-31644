0938504db220ffbca1495f2553b3f38bb7d08763
massun1999 <73097630+massun1999@users.noreply.github.com>
2020-11-09 18:50:41 +0900

    createアクションの定義

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -4,7 +4,26 @@ class PurchasesController < ApplicationController
     @product = Product.find(params[:product_id])
   end
 
+  def new
+    @purchase_address = PurchaseAddress.new(purchase_params)
+  end
+
   def create
+    @purchase_address = PurchaseAddress.new(purchase_params)
+    if @purchase_address.valid?
+      @purchase_address.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
+    
+  end
+
+
+  private
+
+  def purchase_params
+    params.require(:purchase_address).permit(:postal, :city, :house_number, :building, :phone)
   end
 
 end
