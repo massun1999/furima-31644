a323dded926e1e1a4bd1196d920041cf6bea1e14
massun1999 <73097630+massun1999@users.noreply.github.com>
2020-11-10 07:59:45 +0900

    FacrotyBotを設定

--- spec/factories/purchase_addresses.rb
+++ spec/factories/purchase_addresses.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :purchase_address do
-    
+    postal { '510-1323' }
+    prefecture_id { 3 }
+    city { '菰野町' }
+    house_number { '小島' }
+    building { 'AIコンフォート' }
+    phone { '09063979349' }
   end
 end
