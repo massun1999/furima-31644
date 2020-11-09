881c4bcc9267e1a18fe2a3d684a8dcf75226bfdb
massun1999 <73097630+massun1999@users.noreply.github.com>
2020-11-09 19:40:28 +0900

    バリデーション修正

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -6,14 +6,14 @@ class PurchaseAddress
     validates :postal, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
     validates :city
     validates :house_number
-    validates :phone
+    validates :phone, format: {with: /\A0[5789]0[-]?\d{4}[-]?\d{4}\z/}
   end
 
   validates :prefecture_id, presence: true, numericality: { other_than: 0 }
 
   def save
     Purchase.create(product_id: @product.id)
-    Address.create(:postal, :cith, :houce_number, :building, :phone, purchase_id: purchase.id)
+    Address.create(:postal, :city, :house_number, :building, :phone, purchase_id: purchase.id)
   end
   
 end
