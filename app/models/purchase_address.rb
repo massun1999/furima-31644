0a795826ba2a1458045daa329edc0bd8db25a64f
massun1999 <73097630+massun1999@users.noreply.github.com>
2020-11-10 20:38:05 +0900

    バリデーションの編集

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,19 +1,20 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor :postal, :prefecture_id, :city, :house_number, :building, :phone
+  attr_accessor :postal, :prefecture_id, :city, :house_number, :building, :phone, :token, :product_id, :user_id, :purchase_id
 
   with_options presence: true do
     validates :postal, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
     validates :city
     validates :house_number
     validates :phone, format: {with: /\A0[5789]0[-]?\d{4}[-]?\d{4}\z/}
+    validates :prefecture_id, numericality: { other_than: 0 }
+    validates :token
   end
 
-  validates :prefecture_id, presence: true, numericality: { other_than: 0 }
 
-  def save
-    Purchase.create(product_id: @product.id)
-    Address.create(:postal, :city, :house_number, :building, :phone, purchase_id: purchase.id)
+  def save(purchase_params)
+    purchase = Purchase.create(product_id: product_id, user_id: user_id)
+    Address.create(postal: postal, prefecture_id: prefecture_id, city: city, house_number: house_number, building: building, phone: phone, purchase_id: purchase.id)
   end
   
 end
