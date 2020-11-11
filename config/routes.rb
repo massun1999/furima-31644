681d55943d4e1b5072cb77c7a076bedb5824994b
massun1999 <73097630+massun1999@users.noreply.github.com>
2020-11-11 14:58:19 +0900

    Revert "destroyアクションのルーティングを設定"
    This reverts commit 31c16fa10903af84347b3e57adcb05a8a7c72ef2.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'products#index'
-  resources :products do
+  resources :products, only: [:index, :new, :create, :show, :edit, :update] do
     resources :purchases, only: [:index, :create]
   end
 end
